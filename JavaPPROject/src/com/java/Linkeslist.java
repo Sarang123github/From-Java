6bbfca8cf1636e43a0a14cbe22df4dc751374417
Sarang <bhongsarang007@gmail.com>
2023-03-09 22:01:11 +0530

    addded code for lim=nked list

---
+++ JavaPPROject/src/com/java/Linkeslist.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Linkeslist {
+
+	public static void main(String[] args) {
+		List<Integer> l=new LinkedList <Integer>();
+		
+		l.add(2);
+		l.add(33);
+		
+		System.out.println(l);
+		
+
+	}
+	}
+
+
