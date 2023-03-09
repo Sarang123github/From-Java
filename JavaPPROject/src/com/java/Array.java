167fbbabc0c53f616b20a3f4e1ce880510bb2d44
Sarang <bhongsarang007@gmail.com>
2023-03-09 21:55:25 +0530

    arraylist code is add

---
+++ JavaPPROject/src/com/java/Array.java
@@ -0,0 +1,20 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		List<Integer> l=new ArrayList <Integer>();
+		
+		l.add(2);
+		l.add(33);
+		
+		System.out.println(l);
+		
+
+	}
+
+}
